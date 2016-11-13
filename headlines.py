496ec7379d1463723b4a20c4f3333aebde9bf72c
Mike Ubuntu1 Dev <mike@Ubunut1>
2016-11-13 09:02:14 +0100

    one rss version

---
+++ headlines.py
@@ -0,0 +1,22 @@
+import feedparser
+from flask import Flask
+
+app = Flask(__name__)
+
+BBC_FEED = "http://feeds.bbci.co.uk/news/science_and_environment/rss.xml"
+
+@app.route("/")
+def get_news():
+   feed = feedparser.parse(BBC_FEED)
+   first_article = feed['entries'][1]
+   return """<html>
+      <body>
+         <h1> BBC Headlines </h1>
+         <b>{0}</b> <br/>
+         <i>{1}</i> <br/>
+         <p>{2}</p> <br/>
+      </body>
+   </html>""".format(first_article.get("title"),first_article.get("published"), first_article.get("summery"))
+
+if __name__ == '__main__':
+   app.run(port=5000, debug=True)
