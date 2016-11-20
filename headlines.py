956f3160075812be1f70a2d8ebe6ee40571c4d37
Mike Ubuntu1 Dev <mike@Ubunut1>
2016-11-20 19:33:06 +0100

    drop-down get currency & weather

--- headlines.py
+++ headlines.py
@@ -7,7 +7,9 @@ from flask import Flask
 from flask import request
 
 DEFAULTS = {'publication':'bbc',
-            'city':'London,UK'}
+            'city':'London,UK',
+            'currency_from':'GBP',
+            'currency_to':'USD'}
 
 app = Flask(__name__)
 
@@ -16,6 +18,7 @@ RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/science_and_environment/rss.xml
              'au':'http://www.dailytelegraph.com.au/news/breaking-news/rss'}
 
 WEATHER_URL='http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=59bde91a9f41d926ad4a94ea3d935d40'
+CURRENCY_URL='https://openexchangerates.org/api/latest.json?app_id=0d1191c9362441e1b939d55f537efe30'
 
 @app.route("/")
 def home():
@@ -28,9 +31,24 @@ def home():
    if not city:
       city = DEFAULTS['city']
    weather = get_weather(city)
-   return render_template("home.html", articles=articles,weather=weather)
+   # get customized based on user input or default
+   currency_from = request.args.get("currency_from")
+   if not currency_from:
+      currency_from = DEFAULTS['currency_from']
+   currency_to = request.args.get("currency_to")
+   if not currency_to:
+      currency_to = DEFAULTS['currency_to']
+   rate, currencies = get_rate(currency_from, currency_to)
+ #  rate = get_rate(currency_from, currency_to)
+   return render_template("home.html", articles=articles,weather=weather,currency_from=currency_from,currency_to=currency_to, rate=rate,currencies=sorted(currencies))
 
- 
+def get_rate(frm,to):
+   all_currency = urllib2.urlopen(CURRENCY_URL).read()
+   parsed = json.loads(all_currency).get('rates')
+   frm_rate = parsed.get(frm.upper())
+   to_rate = parsed.get(to.upper()) 
+   return (to_rate/frm_rate,  parsed.keys())
+
 def get_weather(query):
    query = urllib.quote(query)
    url = WEATHER_URL.format(query)
@@ -38,7 +56,7 @@ def get_weather(query):
    parsed = json.loads(data)
    weather = None
    if parsed.get("weather"):
-      weather = {"description":parsed["weather"][0]["description"],"temperature":parsed["main"]["temp"],"city":parsed["name"],"lat":parsed["coord"]["lat"],"lon":parsed["coord"]["lon"],"humidity":parsed["main"]["humidity"],"pressure":parsed["main"]["pressure"],"temp_min":parsed["main"]["temp_min"],"temp_max":parsed["main"]["temp_max"],"wind":parsed["wind"]["speed"]}
+      weather = {"description":parsed["weather"][0]["description"],"temperature":parsed["main"]["temp"],"city":parsed["name"],"lat":parsed["coord"]["lat"],"lon":parsed["coord"]["lon"],"humidity":parsed["main"]["humidity"],"pressure":parsed["main"]["pressure"],"temp_min":parsed["main"]["temp_min"],"temp_max":parsed["main"]["temp_max"],"wind":parsed["wind"]["speed"],"country":parsed["sys"]["country"]}
 
    return weather
 
