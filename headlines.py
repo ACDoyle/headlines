0f82c712df2bc4cfc424ec7de49fe33aa504b2ef
Mike Ubuntu1 Dev <mike@Ubunut1>
2016-11-20 14:49:27 +0100

    variable location, podcast

--- headlines.py
+++ headlines.py
@@ -6,16 +6,34 @@ from flask import render_template #Jinja temlate use, converts to html
 from flask import Flask
 from flask import request
 
+DEFAULTS = {'publication':'bbc',
+            'city':'London,UK'}
+
 app = Flask(__name__)
 
 RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/science_and_environment/rss.xml',
              'cnn':'http://rss.cnn.com/rss/edition_technology.rss',
              'au':'http://www.dailytelegraph.com.au/news/breaking-news/rss'}
 
+WEATHER_URL='http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=59bde91a9f41d926ad4a94ea3d935d40'
+
+@app.route("/")
+def home():
+   #get customized headlines, based on user input or defaults
+   publication = request.args.get('publication')
+   if not publication:
+      publication = DEFAULTS['publication']
+   articles = get_news(publication)
+   city = request.args.get('city')
+   if not city:
+      city = DEFAULTS['city']
+   weather = get_weather(city)
+   return render_template("home.html", articles=articles,weather=weather)
+
+ 
 def get_weather(query):
-   api_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=59bde91a9f41d926ad4a94ea3d935d40'
    query = urllib.quote(query)
-   url = api_url.format(query)
+   url = WEATHER_URL.format(query)
    data = urllib2.urlopen(url).read()
    parsed = json.loads(data)
    weather = None
@@ -23,16 +41,13 @@ def get_weather(query):
       weather = {"description":parsed["weather"][0]["description"],"temperature":parsed["main"]["temp"],"city":parsed["name"]}
    return weather
 
-@app.route("/")
-def get_news():
-   query = request.args.get("publication")
+def get_news(query):
    if not query or query.lower() not in RSS_FEEDS:
-      publication = "bbc"
+      publication = DEFAULTS["publication"]
    else:
       publication = query.lower()
    feed = feedparser.parse(RSS_FEEDS[publication])
-   weather = get_weather("London,UK")
-   return render_template("home.html", articles=feed["entries"],weather=weather)
+   return feed['entries']
 
 if __name__ == '__main__':
    app.run(port=5000, debug=True)
