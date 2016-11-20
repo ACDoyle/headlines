73267cdc5fbd542bdae21c88c27a2f565ac279dd
Mike Ubuntu1 Dev <mike@Ubunut1>
2016-11-20 14:34:59 +0100

    simple weather

--- headlines.py
+++ headlines.py
@@ -1,4 +1,7 @@
 import feedparser
+import json
+import urllib2
+import urllib
 from flask import render_template #Jinja temlate use, converts to html
 from flask import Flask
 from flask import request
@@ -9,6 +12,17 @@ RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/science_and_environment/rss.xml
              'cnn':'http://rss.cnn.com/rss/edition_technology.rss',
              'au':'http://www.dailytelegraph.com.au/news/breaking-news/rss'}
 
+def get_weather(query):
+   api_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=59bde91a9f41d926ad4a94ea3d935d40'
+   query = urllib.quote(query)
+   url = api_url.format(query)
+   data = urllib2.urlopen(url).read()
+   parsed = json.loads(data)
+   weather = None
+   if parsed.get("weather"):
+      weather = {"description":parsed["weather"][0]["description"],"temperature":parsed["main"]["temp"],"city":parsed["name"]}
+   return weather
+
 @app.route("/")
 def get_news():
    query = request.args.get("publication")
@@ -17,7 +31,8 @@ def get_news():
    else:
       publication = query.lower()
    feed = feedparser.parse(RSS_FEEDS[publication])
-   return render_template("home.html", articles=feed['entries'])
+   weather = get_weather("London,UK")
+   return render_template("home.html", articles=feed["entries"],weather=weather)
 
 if __name__ == '__main__':
    app.run(port=5000, debug=True)
